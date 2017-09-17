eb9786850c9d8b04acae8bb79225fb8e5049a3c4
3goliad <3goliad@gmail.com>
2017-09-17 17:06:42 -0400

    added lighting and rendering a single edge

--- src/growing_chains/core.cljs
+++ src/growing_chains/core.cljs
@@ -8,7 +8,7 @@
                                    (/ (.-innerWidth js/window)
                                       (.-innerHeight js/window))
                                    1 1000)
-    (.. -position (set 500 100 500))))
+    (.. -position (set 500 0 0))))
 
 (defn get-renderer
   []
@@ -20,30 +20,62 @@
 (def renderer (get-renderer))
 (def scene (js/THREE.Scene.))
 (def camera (get-camera))
-
-(.add scene (js/THREE.AmbientLight.))
-
-(defn create-rect
-  "Creates a rect with given color and xyz."
-  [x y z]
-  (js/THREE.Mesh. (js/THREE.BoxGeometry. x y z)
-                  (js/THREE.MeshBasicMaterial.)))
-
-; Creates white rect:
-(def rect (create-rect 100 100 100))
-(.. rect -position (set 0 0 0))
-(.add scene rect)
-
 (.lookAt camera (js/THREE.Vector3.))
+
+(defn get-spotlight
+  [x y z intensity]
+  (let [light (js/THREE.SpotLight.
+               0x2c61ac
+               1.0
+               1000
+               (/ (.-PI js/Math) 2)
+               1.0
+               1)]
+    (.set (.-position light) x y z)
+    (aset light "castShadow" true)
+    light))
+
+(def key-light (get-spotlight 300 100 200 2))
+(def fill-light (get-spotlight 300 0 -200 0.5))
+(def back-light (get-spotlight -200 0 300 0.5))
+
+(.add scene key-light)
+(.add scene fill-light)
+(.add scene back-light)
+
+(defn create-shape
+  [heights]
+  (doto (js/THREE.Shape.)
+    (.moveTo 0 0)
+    (.lineTo 0 (first heights))
+    (.splineThru (to-array (keep-indexed (fn [x y] (js/THREE.Vector2. (* 10 x) y)) heights)))
+    (.moveTo (dec (count heights)) 0)
+    (.closePath)))
+
+(defn create-side
+  [heights]
+  (let [shape (create-shape heights)]
+    (js/THREE.ShapeGeometry. shape)))
+
+(def side1 (create-side (map (fn [x] (* x 10)) [2 3 4 5 6 2 3 4 6 8 3 4 5 7 9 3 4 5 6 8 9 4 3 5 6 8 9 4 2])))
+
+(defn create-mesh
+  [geometry]
+  (js/THREE.Mesh. geometry (js/THREE.MeshPhongMaterial.)))
+
+
+(def object (create-mesh side1))
+(.. object -position (set 0 0 0))
+(.add scene object)
 
 (defn do-render
   "Called on each render."
   []
   ; Rotates white rect:
-  (set! (.. rect -rotation -x)
-        (+ (.. rect -rotation -x) 0.01))
-  (set! (.. rect -rotation -y)
-        (+ (.. rect -rotation -y) 0.01)))
+  (set! (.. object -rotation -x)
+        (+ (.. object -rotation -x) 0.01))
+  (set! (.. object -rotation -y)
+        (+ (.. object -rotation -y) 0.01)))
 
 (defn render
   "Called on each render. This function does not reload on change."
